52dbef2e57aa1019e700e2c3fde6aefd0dc21058
Addie Cox <Addie Cox@DESKTOP-NF4CRLO>
2019-04-27 17:28:35 -0700

    AddiE

--- DegreeCandidacy/DegreeCandidacy/XSupplyChain.xaml.cs
+++ DegreeCandidacy/DegreeCandidacy/XSupplyChain.xaml.cs
@@ -24,9 +24,24 @@ namespace DegreeCandidacy
             InitializeComponent();
         }
 
+        public IEnumerable<Control> Control { get; private set; }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
+            int SCM3223 = 3;
+            foreach (Control control in this.Control)               
+                if (control is CheckBox)
+                {
+                    SCM3223 = SCM3223 + Convert.ToInt32(control.Tag);
+                    lstTotalHours.Items.Add(SCM3223);
+                }
+            }
+
+
+
+
+
         }
     }
-}
+
