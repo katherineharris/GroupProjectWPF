0ff78b352da0b47f11ef908e8666b40f44259b22
Addie Cox <Addie Cox@DESKTOP-NF4CRLO>
2019-04-28 09:43:33 -0700

    Adding buttons

--- DegreeCandidacy/DegreeCandidacy/XSupplyChain.xaml.cs
+++ DegreeCandidacy/DegreeCandidacy/XSupplyChain.xaml.cs
@@ -31,6 +31,8 @@ namespace DegreeCandidacy
 
             if ((SCM3223.IsChecked == true) || (SCM3323.IsChecked == true) || (SCM3523.IsChecked == true) || (SCM4003.IsChecked == true) | (SCM4013.IsChecked == true) || (SCM4323.IsChecked == true) || (scmElective1.IsChecked == true) || (scmElective2.IsChecked == true) || (scmElective3.IsChecked == true) || (scmElective4.IsChecked == true) || (scmElective5.IsChecked == true))
             {
+                //need to add if checked
+                //assign value to each cheched as 3?
                 Window3 win3 = new Window3();
                 win3.Show();
                 this.Close();
